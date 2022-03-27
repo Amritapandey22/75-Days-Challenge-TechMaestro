f3526968317055e6bde8c7f123705e773325d78f
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-03-27 22:52:33 +0530

    Time: 287 ms (66.47%), Space: 96.9 MB (84.69%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -1,36 +1,47 @@
 #define pb push_back
 class RandomizedSet {
 public:
+    RandomizedSet() {
+       
+    }
     unordered_map<int,int>mp;
     vector<int>nums;
-    RandomizedSet() {
-        
-    }    
+    int idx;
     bool insert(int val) {
         if(mp.find(val)!=mp.end()) return false;
         
         mp[val]=nums.size();
+        
         nums.pb(val);
         
         return true;
-    }    
+        
+    }
+    
     bool remove(int val) {
         if(mp.find(val)==mp.end())return false;
-        int remidx=mp[val];
-        mp.erase(val);
-        if(remidx==nums.size()-1){
+        idx=mp[val];
+        if(idx==nums.size()-1){
             nums.pop_back();
+            mp.erase(val);
             return true;
         }
-        swap(nums[remidx],nums[nums.size()-1]);
-        //int nval=nums[nums.size()-1];
-        mp[nums[remidx]]=remidx;
+        swap(nums[idx],nums[nums.size()-1]);
+        mp[nums[idx]]=idx;
+        mp.erase(val);
         nums.pop_back();
         return true;
-        
-    }    
+    }
+    
     int getRandom() {
         return nums[rand()%nums.size()];
     }
 };
 
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
