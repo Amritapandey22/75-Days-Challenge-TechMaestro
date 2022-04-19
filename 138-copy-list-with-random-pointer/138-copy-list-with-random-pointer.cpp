8ee0a1a75e1b0e2970d8e5fc11e15871012a4466
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-04-19 17:16:32 +0530

    Time: 10 ms (65.62%), Space: 11.1 MB (98.43%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,48 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
+
 
 class Solution {
 public:
+    //soln using constant extra space
     Node* copyRandomList(Node* head) {
         if(!head)return head;
-        unordered_map<Node*,Node*>mp;
-        Node *curr=head;
-        Node *chead=new Node(curr->val);
-        Node *temp=chead;
+        Node *curr=head,*forw,*iter;
+        //creating copy nodes
+        while(curr){
+            forw=curr->next;
+            Node *cnode=new Node(curr->val);
+            curr->next=cnode;
+            cnode->next=forw;
+            curr=forw;
+        }
+        curr=head;
+        //assigning of rndm ptr of duplicated nodes
+        while(curr){
+           if(curr->random!=NULL){
+               curr->next->random=curr->random->next;
+           }
+            curr=curr->next->next;
+        }
+        curr=head;
+        Node *chead=curr->next;
+        Node *temp=curr->next;
         
         while(curr){
-            curr=curr->next;
-            temp->next=curr ? new Node(curr->val):NULL;
+            forw=temp->next;            
+            temp->next=forw?forw->next:NULL;
+            curr->next=forw;
+            curr=forw;
             temp=temp->next;
-        }
-        curr=head;
-        temp=chead;
-        while(curr){
-            mp[curr]=temp;
-            curr=curr->next;
-            temp=temp->next;
-        }
-        curr=head;
-        temp=chead;
-        while(curr){
-            Node *t=curr->random;
-            Node *p=t?mp[t]:NULL;
-            temp->random=p;
-            curr=curr->next;
-            temp=temp->next;            
         }
         return chead;
     }
