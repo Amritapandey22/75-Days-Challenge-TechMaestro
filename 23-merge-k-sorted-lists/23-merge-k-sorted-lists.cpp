fdf8a5ecfbf5dd1f71f533146b566284bb87b3b0
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-04-21 21:10:25 +0530

    Time: 36 ms (50.06%), Space: 13.3 MB (74.26%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,41 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* merge2(ListNode* l1,ListNode* l2){
-        if(l1==nullptr or l2==nullptr)return l1==nullptr?l2:l1;
-        ListNode* dummy=new ListNode(-1);
-        ListNode* temp=dummy;
-        while(l1 and l2){
-            if(l1->val<l2->val){
-                temp->next=l1;
-                l1=l1->next;
-                
-            }else{
-                temp->next=l2;
-                l2=l2->next;
-            }
-            
-            temp=temp->next;
+    struct comp{
+        bool operator()(const ListNode *l,const ListNode* r){
+            return l->val>r->val;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+      priority_queue<ListNode*,vector<ListNode*>,comp>pq;
+        
+        for(auto l:lists){
+            if(l)pq.push(l);
+        }
+        if(pq.empty())return NULL;
+        ListNode* res=pq.top();
+        pq.pop();
+        if(res->next)pq.push(res->next);
+        ListNode *t=res;
+        while(pq.size()!=0){
+            t->next=pq.top();
+            pq.pop();
+            t=t->next;
+            if(t->next)pq.push(t->next);
         }
         
-        if(l1)temp->next=l1;
-        if(l2)temp->next=l2;
-        
-        return dummy->next;
-    }
-     ListNode* mergeKLists(vector<ListNode*>& lists,int si,int ei){
-         if(si==ei)return lists[si];
-         int mid=si+(ei-si)/2;
-         
-         ListNode* lh=mergeKLists(lists,si,mid);
-         ListNode* rh=mergeKLists(lists,mid+1,ei);
-         
-         return merge2(lh,rh);
-     }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0)return nullptr;
-        int n=lists.size();
-        return mergeKLists(lists,0,n-1);
-        
+        return res;
     }
 };
