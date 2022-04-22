84d6947644b33e1e0bd7fd9bccdedfcdbea7320b
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-04-22 19:22:21 +0530

    Time: 34 ms (82.46%), Space: 57.8 MB (71.34%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,24 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    TreeNode* first;
+    TreeNode* mid;
+    TreeNode* last;
+    TreeNode* prev;
+    
 public:
-	TreeNode* firstMistake, *secondMistake, *pre;
-	void recoverTree(TreeNode* root) {
-		pre = new TreeNode(INT_MIN);
-		inorder(root);
-		swap(firstMistake->val, secondMistake->val);
-	}
-
-	void inorder(TreeNode* root) {
-		if(root == nullptr) 
-			return;
-
-		inorder(root->left);
-
-		if(firstMistake == nullptr && root->val < pre->val)
-			firstMistake = pre;
-		if(firstMistake != nullptr && root->val < pre->val)
-			secondMistake = root;
-		pre = root;
-
-		inorder(root->right);
-	}
+    void inorder(TreeNode* root){
+        if(root==NULL)return;
+        inorder(root->left);
+        if(prev!=NULL and root->val<prev->val){
+            if(!first){
+                //first violation
+                first=prev;
+                mid=root;
+            }else last=root;
+        }
+        
+        prev=root;
+        inorder(root->right);
+    }
+     void recoverTree(TreeNode* root) {
+      first=mid=last=prev=NULL;
+         inorder(root);
+         if(first and last)swap(first->val,last->val);
+         else swap(first->val,mid->val);
+    }
 };
