118ad24b1724e5322201e4afcbcb8c5fbcabe203
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-05-28 22:50:10 +0530

    Time: 30 ms (40.13%), Space: 18 MB (65.37%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(auto num:nums){
+            sum+=num;
+        }
+        
+        return (n*(n+1))/2-sum;
+    }
+};
