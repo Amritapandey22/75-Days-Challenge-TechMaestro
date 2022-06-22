a43d596f3904d8d5069ac216435b3ee54e6ff3a7
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-22 12:18:40 +0530

    Time: 3 ms (96.67%), Space: 7.2 MB (19.35%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -15,4 +15,18 @@ public:
         
         return true and mp.size()==st.size();
     }
+    /*
+    //amazing solution 
+    
+     bool isIsomorphic(string s, string t) {
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+    
+    */
 };
