b1b45c728384e1972072a9c0b267bc5b76696c9d
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-24 00:54:46 +0530

    Time: 279 ms (79.51%), Space: 63.3 MB (33.94%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -6,7 +6,7 @@ public:
          sort(courses.begin(), courses.end(), [](auto &a, auto &b) {return a[1] < b[1];});
         for(auto d:courses){
             int dur=d[0],endDate=d[1];
-            if(dur<=endDate){
+            //if(dur<=endDate){
                 if(dur+time<=endDate){
                     pq.push(dur);
                     time+=dur;
@@ -18,7 +18,7 @@ public:
                         pq.push(dur);
                     }
                 }
-            }
+            //}
         }
         
         return pq.size();
