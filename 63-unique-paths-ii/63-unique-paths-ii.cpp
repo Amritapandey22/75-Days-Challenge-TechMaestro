acf8b9861242fd0fea3435aacd1e710c0b4055dc
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-30 15:23:44 +0530

    Time: 4 ms (64.01%), Space: 7.9 MB (12.36%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int getPaths(int sr,int sc,int er,int ec,vector<vector<int>>dir,vector<vector<int>>&dp,vector<vector<int>>obGrid){
-     for(int i=er;i>=sr;i--){
-         for(int j=ec;j>=sc;j--){
-             if(i==er and j==ec){
-                 dp[i][j]=1;
-                 continue;
-             }
-            long int count = 0;
-                 for(auto d:dir){
-                 int r=i+d[0],c=j+d[1];
-                 if(r >= 0 and c >= 0 and r <= er and c <= ec and obGrid[r][c]==0) count+=dp[r][c];
-             }
-             
-             dp[i][j]=count;
-         }
-     } 
-        return dp[sr][sc];
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obGrid) {
-        int n= obGrid.size(),m=obGrid[0].size();
-        if(obGrid[0][0]==1 or obGrid[n-1][m-1] == 1) 
-           return 0;
-        vector<vector<int>>dir;
-        dir = {{0,1},{1,0}};
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        return getPaths(0,0,n-1,m-1,dir,dp,obGrid);
+    int solve(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& og){
+        if(i<0 or j<0)return 0;
+        if(og[i][j]==1)return 0;
+        if(i==0 and j==0)return 1;
         
+        if(dp[i][j]!=-1)return dp[i][j];
+        return dp[i][j]=solve(i-1,j,dp,og)+solve(i,j-1,dp,og);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& og) {
+        
+        int n=og.size();
+        int m=og[0].size();
+        if(n==1 and m==1 and og[0][0]==1)return 0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        
+        return solve(n-1,m-1,dp,og);
     }
 };
