b2be75fcae8b42b39001894e4dde7b80a5942085
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-30 21:33:13 +0530

    Time: 15 ms (40.00%), Space: 10.8 MB (55.75%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char slowestKey(vector<int>& rt, string kp) {
+        
+        vector<int>frq(26,0);
+        frq[kp[0]-'a']=rt[0];
+        
+        for(int i=1;i<rt.size();i++){
+            char ch=kp[i];
+            int dur=rt[i]-rt[i-1];
+            frq[ch-'a']=max(frq[ch-'a'],dur);
+        }
+        char ans;
+        int maxi=0;
+        for(int i=0;i<26;i++){
+           if(frq[i]>=maxi){
+               maxi=frq[i];
+               ans=i+'a';
+           } 
+        }
+        
+        return ans;
+    }
+};
