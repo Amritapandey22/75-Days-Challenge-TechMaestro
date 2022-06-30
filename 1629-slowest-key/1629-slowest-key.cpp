50a7ed4394005609eb5cf5d8f3b157c67e83de66
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-30 21:49:03 +0530

    Time: 15 ms (40.00%), Space: 10.8 MB (55.75%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.cpp
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     char slowestKey(vector<int>& rt, string kp) {
-        //this is space consuming approach
-        vector<int>frq(26,0);
-        frq[kp[0]-'a']=rt[0];
+        int diff=rt[0];
+        char key=kp[0];
         
         for(int i=1;i<rt.size();i++){
-            char ch=kp[i];
-            int dur=rt[i]-rt[i-1];
-            frq[ch-'a']=max(frq[ch-'a'],dur);
-        }
-        char ans;
-        int maxi=0;
-        for(int i=0;i<26;i++){
-           if(frq[i]>=maxi){
-               maxi=frq[i];
-               ans=i+'a';
-           } 
+            if(rt[i]-rt[i-1]==diff){
+                key=max(key,kp[i]);
+            }else if(rt[i]-rt[i-1]>diff){
+                diff=rt[i]-rt[i-1];
+                key=kp[i];
+            }
         }
         
-        return ans;
+        return key;
     }
 };
