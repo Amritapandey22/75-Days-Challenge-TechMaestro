34e4f1d40548dd0b61c1a9223ecc9b5c4851d692
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-06-30 15:30:06 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (12.36%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -13,8 +13,21 @@ public:
         int n=og.size();
         int m=og[0].size();
         if(n==1 and m==1 and og[0][0]==1)return 0;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        if(og[0][0]==1)return 0;
+        dp[0][0]=1;
         
-        return solve(n-1,m-1,dp,og);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 and j==0)continue;
+                int count=0;
+                if(i>0 and og[i][j]!=1)count+=dp[i-1][j];
+                if(j>0 and og[i][j]!=1)count+=dp[i][j-1];
+                
+                dp[i][j]=count;
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
