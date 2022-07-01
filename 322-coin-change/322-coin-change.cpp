faac8f2c5342819de5e397800d3b37078cc0e542
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-07-01 13:41:22 +0530

    Time: 135 ms (49.04%), Space: 45.7 MB (18.44%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int Max = amount + 1;
-        vector<int> dp(amount + 1, Max);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
+    int solve(int idx,int trg,vector<int>& coins,vector<vector<int>>&dp){
+        
+        if(idx==0){
+            if(trg%coins[0]==0) return trg/coins[0];
+            return 1e9;
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        
+        if(dp[idx][trg]!=-1)return dp[idx][trg];
+        
+        int nt=0+solve(idx-1,trg,coins,dp);
+        int t=1e9;
+        if(coins[idx]<=trg)t=1+solve(idx,trg-coins[idx],coins,dp);
+        
+        return dp[idx][trg]=min(nt,t);
+    }
+    int coinChange(vector<int>& coins, int trg) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(trg+1,-1));
+        int ans=solve(n-1,trg,coins,dp);
+        
+        return ans==1e9?-1:ans;
     }
 };
