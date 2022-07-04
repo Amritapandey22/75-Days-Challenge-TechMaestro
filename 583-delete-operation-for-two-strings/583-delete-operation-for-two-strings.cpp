fad182359edde2f535ced91a78f846a5dd6d309a
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-07-04 21:33:03 +0530

    Time: 65 ms (19.43%), Space: 12.4 MB (11.69%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-     int getLCS(int n,int m,string &t1, string &t2,vector<vector<int>>&dp){
-        if(n==0 or m==0)return dp[n][m]=0;
-        if(dp[n][m]!=-1)return dp[n][m];
+    int solve(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+        if(i<0 or j<0)return 0;
         
-        int a=getLCS(n-1,m-1,t1,t2,dp);
-        int b=getLCS(n-1,m,t1,t2,dp);
-        int c=getLCS(n,m-1,t1,t2,dp);
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1[i]==s2[j])return dp[i][j]=1+solve(i-1,j-1,s1,s2,dp);
         
-        if(t1[n-1]==t2[m-1])return dp[n][m]=a+1;
-        else return dp[n][m]=max(b,c);
+        return dp[i][j]=max(solve(i,j-1,s1,s2,dp),solve(i-1,j,s1,s2,dp));
     }
-    int minDistance(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return n+m-2*getLCS(n,m,text1,text2,dp);
-        
+    int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<int>prev(m+1,0),curr(m+1,0);
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//               if(s1[i-1]==s2[j-1])curr[j]=1+prev[j-1];
+//                 else curr[j]=max(curr[j-1],prev[j]);
+//             }
+            
+//             prev=curr;
+//         }
+        int len=solve(n-1,m-1,s1,s2,dp);
+       // cout<<len;
+        return (n+m-2*len);
     }
 };
