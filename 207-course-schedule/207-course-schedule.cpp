54dcc5326e2430a4dc84cee0cb36578c3a6c9852
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2022-07-27 18:58:43 +0530

    Time: 43 ms (30.20%), Space: 14.7 MB (25.91%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>&graph,vector<int>&vis,int src){
+        vis[src]=1;
+        bool ans=false;
+        for(auto it:graph[src]){
+            if(vis[it]==0)ans=ans or dfs(graph,vis,it);
+            if(vis[it]==1)return true;//cycle exists
+            
+            
+        }
+        vis[src]=2;
+        
+        return ans;
+        
+    }
+    bool canFinish(int n, vector<vector<int>>& prq) {
+        vector<int>vis(n,0);
+        //0-not visit 1-in current cycle,2-just visited
+        
+        //1->0
+        vector<vector<int>>graph(n);
+        for(auto vec:prq){
+            graph[vec[1]].push_back(vec[0]);
+        }
+        
+        bool ans=false;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                ans=ans or dfs(graph,vis,i);
+            }
+        }
+        
+        return !ans;
+    }
+};
